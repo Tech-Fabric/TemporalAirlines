34f468f80502632d51ae9189d9ade45c38985111
Sergey Ustimenko <sergey@ustimenko.email>
2024-01-16 20:00:25 +0100

    Web project!

---
+++ TemporalAirlinesConcept.Web/Program.cs
@@ -0,0 +1,46 @@
+using TemporalAirlinesConcept.Api.Configuratoin;
+using TemporalAirlinesConcept.Configuration.ConfiguratoinExtensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureServices(builder.Configuration);
+builder.Services.ConfigureTemporalClient();
+
+// Worker for testing
+builder.Services.ConfigureTemporalWorker();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDefaultUsers();
+}
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.UseStaticFiles();
+
+app.Run();
