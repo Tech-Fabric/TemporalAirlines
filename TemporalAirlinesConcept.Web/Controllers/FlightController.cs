f3e6898e016f390c1983458df7f07d3a2701aa0b
Sergey Ustimenko <sergey@ustimenko.email>
2024-02-07 17:45:40 +0100

    UI improvemets - CC screen, seat selection screen

--- TemporalAirlinesConcept.Web/Controllers/FlightController.cs
+++ TemporalAirlinesConcept.Web/Controllers/FlightController.cs
@@ -1,18 +1,17 @@
 ﻿using Htmx;
 using Microsoft.AspNetCore.Mvc;
-using TemporalAirlinesConcept.Services.Implementations.Purchase;
+using TemporalAirlinesConcept.Services.Interfaces.Purchase;
 using TemporalAirlinesConcept.Services.Models.Purchase;
 using TemporalAirlinesConcept.Web.ViewComponents;
 
 namespace TemporalAirlinesConcept.Web.Controllers;
 
-[ApiController]
 [Route("flights")]
 public class FlightController : Controller
 {
-    private readonly TicketService _ticketService;
+    private readonly ITicketService _ticketService;
 
-    public FlightController(TicketService ticketService)
+    public FlightController(ITicketService ticketService)
     {
         _ticketService = ticketService;
     }
@@ -30,22 +29,20 @@ public class FlightController : Controller
         }
     }
 
-    [HttpPost("form")]
-    public async Task<IActionResult> Form([FromForm] FlightBookingFormViewModel model, [FromQuery] string? selectedFlight)
+    [HttpGet("{SelectedFlight}")]
+    public async Task<IActionResult> Flight([FromRoute] string? selectedFlight)
+    {
+        FlightBookingFormViewModel model = new FlightBookingFormViewModel();
+
+        return await Flight(model, selectedFlight);
+    }
+
+    [HttpPost("{SelectedFlight}")]
+    public async Task<IActionResult> Flight([FromForm] FlightBookingFormViewModel model, [FromRoute] string? selectedFlight)
     {
         if (!string.IsNullOrEmpty(selectedFlight))
         {
             model.SelectedFlight = selectedFlight;
-        }
-
-        if (model.SelectedSeats.Values.Any(v => v))
-        {
-            var result = await _ticketService.RequestTicketPurchaseAsync(
-                new PurchaseModel()
-                {
-                    FlightId = model.SelectedFlight
-                }
-            );
         }
 
         if (Request.IsHtmx())
@@ -54,7 +51,108 @@ public class FlightController : Controller
         }
         else
         {
-            return View("~/Views/Home/Index.cshtml", model);
+            return View("~/Views/Flight/Index.cshtml", model);
+        }
+    }
+
+    [HttpPost("{SelectedFlight}/purchase")]
+    public async Task<IActionResult> Purchase(
+        [FromForm] FlightBookingFormViewModel model,
+        [FromRoute] string? selectedFlight
+    ) {
+        if (!string.IsNullOrEmpty(selectedFlight))
+        {
+            model.SelectedFlight = selectedFlight;
+        }
+
+        var purchaseWorkflowId = "";
+
+        if (!string.IsNullOrEmpty(model.CreditCardDetails.CardNumber))
+        {
+            purchaseWorkflowId = await _ticketService.RequestTicketPurchaseAsync(
+                new PurchaseModel()
+                {
+                    FlightId = model.SelectedFlight
+                }
+            );
+
+            // TODO: fetch workflow status
+
+            model.PaymentSuccessful = true;
+        }
+
+        if (Request.IsHtmx())
+        {
+            if (!string.IsNullOrEmpty(purchaseWorkflowId))
+            {
+                Response.Htmx(h =>
+                {
+                    h.PushUrl($"/flights/{model.SelectedFlight}/ticket/{purchaseWorkflowId}");
+                });
+            }
+
+            return ViewComponent(typeof(FlightBookingFormViewComponent), model);
+        }
+        else
+        {
+            return View("~/Views/Flight/Index.cshtml", model);
+        }
+    }
+
+    [HttpGet("{SelectedFlight}/ticket/{PurchaseWorkflowId}")]
+    public async Task<IActionResult> Details(
+        [FromRoute] string? selectedFlight,
+        [FromRoute] string? purchaseWorkflowId
+    )
+    {
+        FlightBookingFormViewModel model = new FlightBookingFormViewModel();
+
+        if (!string.IsNullOrEmpty(selectedFlight))
+        {
+            model.SelectedFlight = selectedFlight;
+        }
+
+
+        if (Request.IsHtmx())
+        {
+            return ViewComponent(typeof(FlightBookingFormViewComponent), model);
+        }
+        else
+        {
+            return View("~/Views/Flight/Index.cshtml", model);
+        }
+    }
+
+    [HttpPost("{SelectedFlight}/ticket/{PurchaseWorkflowId}")]
+    public async Task<IActionResult> Details(
+        [FromForm] FlightBookingFormViewModel model,
+        [FromRoute] string? selectedFlight,
+        [FromRoute] string? purchaseWorkflowId
+    ) {
+        if (!string.IsNullOrEmpty(selectedFlight))
+        {
+            model.SelectedFlight = selectedFlight;
+        }
+
+        var selectedSeatsCount = model.SelectedSeats.Count(kv => kv.Value == true);
+
+        if (selectedSeatsCount > model.NumberOfSeats)
+        {
+            ModelState.AddModelError(nameof(model.NumberOfSeats), "Too many seats selected");
+        }
+
+        if (ModelState.IsValid)
+        {
+
+        }
+
+        if (Request.IsHtmx())
+        {
+            return ViewComponent(typeof(FlightBookingFormViewComponent), model);
+        }
+        else
+        {
+            return View("~/Views/Flight/Index.cshtml", model);
         }
     }
 }
--- TemporalAirlinesConcept.Web/ViewComponents/FlightBookingFormViewModel.cs
+++ TemporalAirlinesConcept.Web/ViewComponents/FlightBookingFormViewModel.cs
@@ -1,6 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using TemporalAirlinesConcept.DAL.Entities;
 
 namespace TemporalAirlinesConcept.Web.ViewComponents;
+
+public class CreditCardDetails
+{
+    public string NameOnCard { get; set; }
+
+    [CreditCard]
+    public string CardNumber { get; set; }
+
+    public int ExpiresMonth { get; set; }
+    public int ExpiresYear { get; set; }
+    public int CVV { get; set; }
+}
 
 public class FlightBookingFormViewModel
 {
@@ -9,9 +22,18 @@ public class FlightBookingFormViewModel
 
     public DateTime? When { get; set; }
 
-    public string? SelectedFlight { get; set; }
-
     public List<Flight>? Flights { get; set; }
     public Dictionary<string,string>? Airports { get; set; }
+
+    public int NumberOfSeats { get; set; }
+
+    public CreditCardDetails? CreditCardDetails { get; set; }
+
+    public string? SelectedFlight { get; set; }
+
+    public bool PaymentSuccessful { get; set; }
+
     public Dictionary<string,bool>? SelectedSeats { get; set; }
+
+    public bool ShowSeatSelectionScreen { get; set; }
 }
