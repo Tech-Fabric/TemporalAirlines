a573c711966f9b93593ea169770e4ccf213506a4
vladyslav.mandryk <vladislav.mandryk@gmail.com>
2024-02-07 13:06:23 +0200

    add-tickets-api

--- TemporalAirlinesConcept.Services/Interfaces/Purchase/ITicketService.cs
+++ TemporalAirlinesConcept.Services/Interfaces/Purchase/ITicketService.cs
@@ -1,8 +1,17 @@
-﻿using TemporalAirlinesConcept.Services.Models.Purchase;
+﻿using TemporalAirlinesConcept.DAL.Entities;
+using TemporalAirlinesConcept.Services.Models.Purchase;
 
 namespace TemporalAirlinesConcept.Services.Interfaces.Purchase;
 
 public interface ITicketService
 {
     Task<string> RequestTicketPurchaseAsync(PurchaseModel purchaseModel);
+
+    Task<List<Ticket>> GetTickets(string userId);
+
+    Task<List<Ticket>> GetTickets(string userId, string flightId);
+
+    Task<Ticket> GetTicket(string ticketId);
+
+    Task MarkAsPaid(string purchaseWorkflowId);
 }
